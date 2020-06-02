55bbff61bc6962ac31851f319f2e4c09fb74262b
kyle Dornan <kyledornan26@gmail.com>
2020-06-02 18:12:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,6 @@
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(ikea)
 ikea.collect do |key_name,key_name2,num|
-  key_name.length<=>key_name2.length 
+  key_name.length<=>key_name.length 
 end
 end 
